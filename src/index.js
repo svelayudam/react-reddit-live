bfe26a17c8830e65f9bd88f82757a16ed1c063fe
Sathish Velayudam <sathish.velayudam@gmail.com>
2020-06-20 21:14:21 -0400

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from "axios";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Reddit() {
+  const [posts, setPosts] = React.useState([]);
+  React.useEffect(() => {
+    axios.get(`https://www.reddit.com/r/reactjs.json`).then(res => {
+      const newPosts = res.data.data.children.map(obj => obj.data);
+      setPosts(newPosts);
+    });
+  }, []);
+  return (
+    <div>
+      <h1>/r/reactjs</h1>
+      <ul>
+        {posts.map(post => {
+          return <li key={post.id}>{post.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Reddit />, document.getElementById("root"));
